6b7f505b7a31b2b12d09709962c91251e0a409d8
adis19 <ask.net.ua@gmail.com>
2023-12-12 19:35:35 +0600

    First commit

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path("",views.main,name = 'Home'),
     path("about/",views.about, name ="About"),
-    path('register/', views.register, name = 'Register' )
+    path('register/', views.register, name = 'Register' ),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile_view, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from .models import *
 from .form import Registration
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def main(request):
@@ -25,3 +27,31 @@ def register(request):
             messages.error (request, 'Form is not correct')
     return render (request,'register.html', {'form':form})
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            # For example: return HttpResponseRedirect('/success/')
+            # Replace '/success/' with your success URL.
+        else:
+            # Return an 'invalid login' error message.
+            # For example: return render(request, 'login.html', {'error_message': 'Invalid login'})
+            # Pass an error message to the template if login fails.
+            return render(request, 'login.html', {'error_message': 'Invalid login'})
+
+    return render(request, 'login.html')
+
+
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Перенаправление на страницу входа после выхода
